bf52b36322019429e9976e02a718c0b00b0cb2a7
thales Nunes <thalesgsn@gmail.com>
2022-06-19 15:50:48 -0300

    Add new files

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -14,7 +14,7 @@
 /* eslint-disable */
 // cypress/plugins/index.js
 module.exports = (on, config) => {
-  require('@cypress/code-coverage/task')(on, config);
-  on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))
+  // require('@cypress/code-coverage/task')(on, config);
+  // on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))
   return config;
 };
